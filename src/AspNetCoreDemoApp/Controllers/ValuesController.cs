9e4038b513c17a386c052f8dc0971e96bacb10be
Jon Woo <jw56578@gmail.com>
2021-03-09 16:13:48 -0600

    does mysql package work on heroku

--- src/AspNetCoreDemoApp/Controllers/ValuesController.cs
+++ src/AspNetCoreDemoApp/Controllers/ValuesController.cs
@@ -1,28 +1,62 @@
 using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreDemoApp.Controllers
 {
-	[Route("api/[controller]")]
-	public class ValuesController : ControllerBase
-	{
-		// GET: api/values
-		[HttpGet]
-		public IEnumerable<string> Get()
-		{
-		    Console.WriteLine(Request.GetDisplayUrl());
-		    Console.WriteLine(Request.GetEncodedUrl());
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Console.WriteLine(Request.GetDisplayUrl());
+            Console.WriteLine(Request.GetEncodedUrl());
 
-			return new[] { "value1", "value2" };
-		}
+            return GetData();
+        }
 
-		// GET api/values/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
-	}
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public static List<string> GetData()
+        {
+            List<string> items = new List<string>();
+            string connStr = "server=den1.mysql1.gear.host;user=aca311week3day1;database=aca311week3day1;port=3306;password=Wq87E!7mUF~7";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * from customers";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    items.Add(rdr[1].ToString());
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return items;
+        }
+    }
 }
